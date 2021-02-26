106dc48c94a9c99d0dac7a3386e3bdef9d0f8547
ANousiainen <77267202+ANousiainen@users.noreply.github.com>
2021-02-26 13:09:25 +0200

    syö kikkeli :-D

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import News from './News';
+import Details from './Details';
+import {StyleSheet} from 'react-native';
 
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="News">
+        <Stack.Screen 
+          name="News"
+          component={News}
+          options={{
+            title: 'News',
+            headerTitle: 'News'
+          }}
+        />
+        <Stack.Screen 
+          name="Details"
+          component={Details}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex:1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 0,
   },
-});
+  news: {
+    padding: 20,
+  }
+})
